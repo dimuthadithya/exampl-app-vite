5ac344a96d16fb1485a9b8515f65e94e55fe61c6
Dimuth Adithya <dimuthx353@gmail.com>
2026-04-07 16:07:12 +0530

    refactor: update App component to display test message

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>hello world</h1>
-      <h2>another change</h2>
+      <h1>this is first automation deployment trigger</h1>
     </>
   );
 }
